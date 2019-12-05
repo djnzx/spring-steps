1bfcdcbf5e78f40c5b2eff67d85ca4a4dbb5c35c
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-05 20:55:22 +0400

    -- cleanup

--- src/main/java/springdata/Application2.java
+++ src/main/java/springdata/Application2.java
@@ -9,12 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application2 {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application2.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application2.class);
     }
 
     @Bean
