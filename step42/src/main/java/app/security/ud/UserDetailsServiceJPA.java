abd489e41860f3c82fc22deed428c51a3bf1d139
Alexey <alexey.rykhalskiy@gmail.com>
2019-12-15 19:39:48 +0400

    -- step42: -- UserDetailsServiceJPA.loadUserById(int id)

--- step42/src/main/java/app/security/ud/UserDetailsServiceJPA.java
+++ step42/src/main/java/app/security/ud/UserDetailsServiceJPA.java
@@ -27,11 +27,27 @@ public class UserDetailsServiceJPA implements UserDetailsService {
         .build();
   }
 
+  /**
+   * we use this method in standard Spring Security authorization mechanism
+   * @throws UsernameNotFoundException
+   */
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     log.info(String.format(">>>>>>> UserDetails.loadUserByUsername:%s", username));
     return dbUserRepo.findByUsername(username).map(UserDetailsServiceJPA::mapper)
         .orElseThrow(() -> new UsernameNotFoundException(
             String.format("User `%s` not found", username)));
+  }
+
+  /**
+   * we will use this method in JWT authorization part
+   * @throws UsernameNotFoundException
+   */
+  public UserDetails loadUserById(int userid) throws UsernameNotFoundException {
+    log.info(String.format(">>>>>>> UserDetails.loadUserById:%d", userid));
+    return dbUserRepo.findById(userid).map(UserDetailsServiceJPA::mapper)
+        .orElseThrow(() -> new UsernameNotFoundException(
+            String.format("User with id:%d` not found", userid)));
+
   }
 }
